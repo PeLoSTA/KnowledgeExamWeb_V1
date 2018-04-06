f2bb4cfd64935900fda4fa007bbdcc3674f92cfd
Peter <peter.loos@gmx.de>
2018-04-06 11:07:07 +0200

    testing UI for questions

---
+++ index7.js
@@ -0,0 +1,24 @@
+/*jslint browser:false */
+/*jslint esnext:true*/
+/*jslint es6 */
+/*global console, document, firebase */
+
+/*global componentHandler */
+
+/*eslint no-console: [ "error", { allow: [  "log", "warn", "error"]}] */
+
+// retrieve HTML elements
+var menuTest = document.getElementById('test-id-menu-entry');
+
+
+// ======================================================================
+// test dialog
+
+menuTest.addEventListener('click', () => {
+
+    'use strict';
+
+    console.log("Wo gehts hier lang "   );
+
+});
+
